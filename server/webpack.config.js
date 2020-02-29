bb7424c1be13a7851df742b3d715a0471bcab4b6
Adil Nurtayev <anurtayev@torstar.ca>
2020-02-29 15:21:23 -0500

    added Apollo server

---
+++ server/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  target: "node",
+  mode: "production",
+  entry: "./index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  watchOptions: {
+    ignored: ["node_modules/**"]
+  },
+  devtool: "eval-source-map"
+};
